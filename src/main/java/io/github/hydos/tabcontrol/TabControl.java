0dc19c48b53ec09f5e376d2472df8639939b2d1b
hayden <haydenv06@gmail.com>
2020-09-04 07:15:20 +1000

    wtf did i do

--- src/main/java/io/github/hydos/tabcontrol/TabControl.java
+++ src/main/java/io/github/hydos/tabcontrol/TabControl.java
@@ -5,13 +5,14 @@ import net.fabricmc.fabric.api.event.lifecycle.v1.ServerPlayerEvents;
 import net.fabricmc.fabric.api.event.server.PlayerConnectCallback;
 import net.minecraft.network.packet.s2c.play.PlayerListHeaderS2CPacket;
 import net.minecraft.text.LiteralText;
+import net.swifthq.swiftapi.callbacks.entity.player.PlayerJoinCallback;
 
 public class TabControl implements DedicatedServerModInitializer {
 
 	@Override
 	public void onInitializeServer() {
-		ServerPlayerEvents.CONNECT.register((connection, player) -> {
-			connection.send(new PlayerListHeaderS2CPacket(new LiteralText("This is a cool test")));
+		PlayerJoinCallback.EVENT.register((player) -> {
+			player.networkHandler.sendPacket(new PlayerListHeaderS2CPacket(new LiteralText("This is a cool test")));
 		});
 	}
 }
---
+++ src/main/java/io/github/hydos/tabcontrol/gson/BasicTabConfig.java
@@ -0,0 +1,10 @@
+package io.github.hydos.tabcontrol.gson;
+
+public class BasicTabConfig {
+
+    public boolean customHeader;
+    public String header;
+
+    public boolean customFooter;
+    public String footer;
+}
